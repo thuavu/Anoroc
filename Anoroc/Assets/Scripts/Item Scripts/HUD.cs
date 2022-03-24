ddc62c04ef657e8890210758bfcddfc17c6d6d7f
Joshua Jan <jan.joshua@yahoo.com>
2022-03-24 09:10:13 -0500

    Inventory

---
+++ Anoroc/Assets/Scripts/Item Scripts/HUD.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Inventory Inventory;
+
+    public GameObject MessagePanel;
+
+    void Start()
+    {
+        Inventory.ItemAdded += InventoryScript_ItemAdded;
+        Inventory.ItemRemoved += InventoryScript_ItemRemoved;
+    }
+
+    private void InventoryScript_ItemAdded(object sender, InventoryEventArgs e)
+    {
+        Transform inventoryPanel = transform.Find("Inventory");
+        foreach(Transform slot in inventoryPanel)
+        {
+            Transform imageTransform = slot.GetChild(0).GetChild(0);
+            Image image = imageTransform.GetComponent<Image>();
+            ItemDragHandler itemDragHandler = imageTransform.GetComponent<ItemDragHandler>();
+
+            if(!image.enabled)
+            {
+                image.enabled = true;
+                image.sprite = e.Item.Image;
+
+                itemDragHandler.Item = e.Item;
+
+                break;
+            }
+        }
+    }
+
+    private void InventoryScript_ItemRemoved(object sender, InventoryEventArgs e)
+    {
+        Transform inventoryPanel = transform.Find("Inventory");
+        foreach (Transform slot in inventoryPanel)
+        {
+            Transform imageTransform = slot.GetChild(0).GetChild(0);
+            Image image = imageTransform.GetComponent<Image>();
+            ItemDragHandler itemDragHandler = imageTransform.GetComponent<ItemDragHandler>();
+
+            if (itemDragHandler.Item.Equals(e.Item))
+            {
+                image.enabled = false;
+                image.sprite = null;
+
+                break;
+            }
+        }
+    }
+
+    public void OpenMessagePanel(string text)
+    {
+        MessagePanel.SetActive(true);
+    }
+
+    public void CloseMessagePanel()
+    {
+        MessagePanel.SetActive(false);
+    }
+}
---
+++ Anoroc/Assets/Scripts/Item Scripts/ItemClickHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemClickHandler : MonoBehaviour
+{
+    public Inventory _Inventory;
+    
+    public void OnItemClicked()
+    {
+        ItemDragHandler dragHandler = gameObject.transform.Find("ItemImage").GetComponent<ItemDragHandler>();
+
+        IInventoryItem item = dragHandler.Item;
+
+        Debug.Log(item.Name);
+
+        _Inventory.UseItem(item);
+
+        item.OnUse();
+    }
+}
